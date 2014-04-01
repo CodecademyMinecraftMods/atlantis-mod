9b012efc40894fbe27f14bdb182ed93606b50854
ETBCOR <Ethan@Corgatelli-Familys-MacBook.local>
2014-03-31 23:08:49 -0600

    Added a few things to clam
    Sry keep pushing to github then realizing that I could have done more…
    and repeat. :/

--- forge-1.6.4-9.11.1.965-src/forge/mcp/src/minecraft/atlantismod/common/entity/EntityClam.java
+++ forge-1.6.4-9.11.1.965-src/forge/mcp/src/minecraft/atlantismod/common/entity/EntityClam.java
@@ -7,6 +7,12 @@ public class EntityClam extends EntityWaterMob {
 
 	public EntityClam(World par1World) {
 		super(par1World);
+        this.tasks.addTask(1, new EntityAIWander(this, 0.0D));
 	}
-
+	public boolean attackEntityAsMob() {
+	return false;
+	}
+	protected void applyEntityAttributes() {
+		
+	}
 }
