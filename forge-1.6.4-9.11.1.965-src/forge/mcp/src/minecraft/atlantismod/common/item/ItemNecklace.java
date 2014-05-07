80e17c27cf11708a340312deaab561c56291e8be
ETBCOR <ETBCOR@gmail.com>
2014-05-07 11:02:02 -0600

    Added Class For Necklace
    Just the base class for necklace with nothing in it yet!

---
+++ forge-1.6.4-9.11.1.965-src/forge/mcp/src/minecraft/atlantismod/common/item/ItemNecklace.java
@@ -0,0 +1,11 @@
+package atlantismod.common.item;
+
+import atlantismod.common.AtlantisMod;
+import net.minecraft.item.Item;
+
+public class ItemNecklace extends Item {
+
+	public ItemNecklace(int par1) {
+		super(par1);
+	}
+}
